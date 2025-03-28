8b908001c59f45bf0076e941ec928809bec7af2b
mrsnailo <shahidparvez.official@gmail.com>
2025-03-28 15:33:22 +0600

    configured main  router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+
+import MainLayout from "./components/Layouts/MainLayout";
+import Feed from "./pages/Feed";
+import Watch from "./pages/Watch";
+import Marketplace from "./pages/Marketplace";
+import Groups from "./pages/Groups";
+import Gaming from "./pages/Gaming";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Feed />,
+        },
+        {
+          path: "feed", 
+          element: <Feed />,
+        },
+        {
+          path: "watch",
+          element: <Watch />,
+        },
+        {
+          path: "marketplace",
+          element: <Marketplace />,
+        },
+        {
+          path: "groups",
+          element: <Groups />,
+        },
+        {
+          path: "gaming",
+          element: <Gaming />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
-      <p className="text-center text-red-200">Welcome To fakebook</p>
+      <RouterProvider router={router} />
     </>
   );
 }
